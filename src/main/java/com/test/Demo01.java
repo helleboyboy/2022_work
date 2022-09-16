9a567c924740cd8b900865918fb416c8ce22ffa9
mr thinker <1786076351@qq.com>
2022-09-16 15:15:17 +0800

    add a line

--- src/main/java/com/test/Demo01.java
+++ src/main/java/com/test/Demo01.java
@@ -7,5 +7,6 @@ public class Demo01 {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger("Demo01.class");
         logger.log(Level.INFO, "test ignore file");
+        logger.log(Level.INFO, "test ignore file");
     }
 }
