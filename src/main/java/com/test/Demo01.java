1d20c5e1815a328c4c8458b333699248004682a3
mr thinker <1786076351@qq.com>
2022-09-16 15:14:02 +0800

    Initial commit

---
+++ src/main/java/com/test/Demo01.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger("Demo01.class");
+        logger.log(Level.INFO, "test ignore file");
+    }
+}
