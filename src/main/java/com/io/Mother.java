cc425ae48e74f67be033d4deb7825587d251fab7
java页 <1786076351@qq.com>
2022-10-15 17:29:31 +0800

    根据报错信息，为mother类提供一个全局变量serialVersionUID（其实参考String类也是有该全局变量的！）以确保能够找到对应的类信息！！！

--- src/main/java/com/io/Mother.java
+++ src/main/java/com/io/Mother.java
@@ -10,8 +10,10 @@ import java.io.Serializable;
  * @Describe:
  *  需要实现Java的序列化条件：
  *      1. 实现Serializable接口
+ *      2. 提供一个全局变量serialVersionUID，确保类信息前后一致！
  */
 public class Mother implements Serializable {
+    private static final long serialVersionUID = -1299546909289720531L;
     private String name;
     private Integer age;
     private Boolean isTeacher;
