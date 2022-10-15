4a8d0ace18e48c5cb51b5e3d232f0adf80cda10c
java页 <1786076351@qq.com>
2022-10-15 17:43:29 +0800

    为MOTHER类的属性值son类实现Serializable接口并提供全局变量serialVersionUID；再测则可以通过。 即类实现序列化需要三个点： 1. 实现Serializable接口 2. 提供一个全局变量serialVersionUID 3. 保证属性均已实现了序列化！！！

--- src/main/java/com/io/Son.java
+++ src/main/java/com/io/Son.java
@@ -1,11 +1,14 @@
 package com.io;
 
+import java.io.Serializable;
+
 /**
  * @Author: Java页大数据
  * @Date: 2022-10-15:17:33
  * @Describe:
  */
-public class Son{
+public class Son implements Serializable {
+    private static final long serialVersionUID = -12995469092831L;
     private Boolean isBoy;
 
     @Override
