d70a41ac53a75c5802055fa5b5ad54a1a62e01ab
java页 <1786076351@qq.com>
2022-10-16 23:50:43 +0800

    引入Socket,实现客户端向服务端发送数据

--- src/main/java/com/inet/InetTest.java
+++ src/main/java/com/inet/InetTest.java
@@ -74,6 +74,105 @@ public class InetTest {
         }
     }
 
+    /**
+     * 引入网络编程的服务端!
+     *  1. 新建ServerSocket实例
+     *  2. 获取客户端Socket
+     *  3. 获取输入流,获取客户端Socket对象的输出流及其内容
+     *  4. 使用 ByteArrayOutputStream 来输出到控制台(ByteArrayOutputStream 可以避免乱码!!!)
+     *  5. 关闭资源,从下而上!
+     */
+    @Test
+    public void server() {
+        int port = 9898;
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+        InputStream socketInputStream = null;
+        ByteArrayOutputStream byteArrayOutputStream = null;
+        try {
+            serverSocket = new ServerSocket(port);
+            socket = serverSocket.accept();
+            socketInputStream = socket.getInputStream();
+            byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[4];
+            int len = 0;
+            while ((len = socketInputStream.read(buffer)) != -1){
+                byteArrayOutputStream.write(buffer, 0, len);
+            }
+            String context = byteArrayOutputStream.toString();
+            String host = socket.getInetAddress().getHostAddress();
+            String res = String.format("客户端%s发来的数据为:%s", host, context);
+            System.out.println(res);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (byteArrayOutputStream != null) {
+                    byteArrayOutputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (socketInputStream != null) {
+                    socketInputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    /**
+     * 测试网络编程的客户端
+     *  1. 新建Socket客户端实例
+     *  2. 获取Socket客户端实例的输出流,并将内容输出到服务端
+     *  3. 关闭资源
+     */
+    @Test
+    public void client() {
+        int serverPort = 9898;
+        Socket clientSocket = null;
+        OutputStream outputStream = null;
+        InetAddress serverInetAddress = null;
+        try {
+            serverInetAddress = InetAddress.getLocalHost();
+            clientSocket = new Socket(serverInetAddress, serverPort);
+            outputStream = clientSocket.getOutputStream();
+            outputStream.write("大家好,我叫java页,别名为javaAndBigdata!".getBytes());
+            System.out.println("客户端已经发送数据!请查看是否收到数据!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
